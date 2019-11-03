0730a71050ccf75f9c6a57eb22bce98d742fd36e
RomanPoghosyan <roman.poghos@gmail.com>
2019-11-03 22:17:52 +0400

    Homework4 recursion

--- JavaScript/Homework4/script.js
+++ JavaScript/Homework4/script.js
@@ -1,13 +1,13 @@
 /* Task sub arrays recursion */
 
 /* Version first, the best I think */
-function subArrays(arr, n, result = [], temp = []){
-    if(n == 0){
+function subArrays(arr, n, result = [], temp = []) {
+    if (n == 0) {
         result.push([...temp]);
         return true;
     }
 
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
         temp.push(arr[i]);
         subArrays(arr.slice(i + 1), n - 1, result, temp);
         temp.pop();
@@ -15,20 +15,20 @@ function subArrays(arr, n, result = [], temp = []){
     return result;
 }
 
-console.log(subArrays([1,2,3,4], 2));
+console.log(subArrays([1, 2, 3, 4], 2));
 
 /* Version second */
-function subArr (arr, n, iterate = true, result = []){
+function subArr(arr, n, iterate = true, result = []) {
     // debugger;
-    if(n == 1){
+    if (n == 1) {
         return arr.map(el => [el]);
-    }else{
-        for(let i = 0; i < arr.length + 1; i++) {
+    } else {
+        for (let i = 0; i < arr.length + 1; i++) {
             let first = [arr[0]];
             arr = arr.slice(1);
-            if(iterate == false){
+            if (iterate == false) {
                 return subArr(arr, n - 1, false, result).map(el => first.concat(el));
-            }else {
+            } else {
                 result.push(...subArr(arr, n - 1, false, result).map(el => first.concat(el)));
             }
         }
@@ -36,17 +36,17 @@ function subArr (arr, n, iterate = true, result = []){
     }
 }
 
-console.log(subArr([1,2,3,4], 2));
+console.log(subArr([1, 2, 3, 4], 2));
 
 
 /* Task 1 */
 
-function greatherThan(arr, num){
+function greatherThan(arr, num) {
     let filtered = arr.filter(n => n > num);
     return filtered.length > 0 ? filtered : "Such values do not exist.";
 }
 
-console.log( greatherThan([7,4,12,5,1], 6) );
+console.log(greatherThan([7, 4, 12, 5, 1], 6));
 
 /* Task 2 */
 
@@ -60,20 +60,20 @@ function onlyEvenDigits(num1, num2) {
 
     let result = [];
 
-    for(let i = num1; i <= num2; i++) {
-        if(allDigitsAreEven(i)) result.push(i);
+    for (let i = num1; i <= num2; i++) {
+        if (allDigitsAreEven(i)) result.push(i);
     }
     return result.length ? result : "Such numbers does not exist.";
 }
 
-console.log(onlyEvenDigits(19,42));
+console.log(onlyEvenDigits(19, 42));
 
 /* Task 3 */
 
-function allDigitsAreOdd(num){
-    if(num == 0) return true;
+function allDigitsAreOdd(num) {
+    if (num == 0) return true;
     let reminder = num % 10;
-    if(reminder % 2 == 0) return false;
+    if (reminder % 2 == 0) return false;
     return allDigitsAreOdd(Math.floor(num / 10));
 }
 
@@ -82,8 +82,8 @@ console.log(allDigitsAreOdd(7139));
 /* Task 4 */
 
 function minPositivNum(arr, index = 0, min = Infinity) {
-    if(index == arr.length - 1) return min == Infinity ? -1 : min;
-    if(arr[index] < min && arr[index] >= 0) min = arr[index];
+    if (index == arr.length - 1) return min == Infinity ? -1 : min;
+    if (arr[index] < min && arr[index] >= 0) min = arr[index];
     return minPositivNum(arr, ++index, min);
 }
 
@@ -91,9 +91,9 @@ console.log(minPositivNum([45, -9, 15, 5, -78]));
 
 /* Task 5 */
 
-function vioaltedOrder(arr){
-    for (let i = 0; i + 1 < arr.length; i++){
-        if(arr[i] > arr[i + 1]) return i + 1;
+function vioaltedOrder(arr) {
+    for (let i = 0; i + 1 < arr.length; i++) {
+        if (arr[i] > arr[i + 1]) return i + 1;
     }
     return -1;
 }
---
+++ JavaScript/Homework5/script.js
@@ -0,0 +1,113 @@
+/* Task 1 */
+
+function removeFirsElement(arr, i = 0, res = []){
+    if(i == arr.length - 1) return res;
+    res[i] = arr[i + 1];
+    return removeFirsElement(arr, i + 1, res);
+}
+
+// console.log(removeFirsElement([7,8,1,2]));
+
+/* Task 2 */
+
+function concatNestedArrays(arr, res = []){
+    for(let item of arr){
+        if(Array.isArray(item)){
+            concatNestedArrays(item, res);
+        }else{
+            res.push(item);
+        }
+    }
+    return res;
+}
+
+// console.log(concatNestedArrays([1, [3, 4, [1, 2]], 10]));
+
+/* Task 3 */
+
+function sumOfDigits(num) {
+    let sum = 0;
+    for(let digit of String(num)){
+        sum += +digit;
+    }
+    if(sum < 10){
+        return sum;
+    } else {
+        return sumOfDigits(sum);
+    }
+}
+
+// console.log(sumOfDigits(29));
+// console.log(sumOfDigits(999999999999));
+
+/* Task 4 */
+
+function rotateArray(arr, n){
+    if(n == 0) return arr;
+    if(n > 0) {
+        arr.push(arr.shift());
+        return rotateArray(arr, n - 1);
+    } else {
+        arr.unshift(arr.pop());
+        return rotateArray(arr, n + 1);
+    }
+}
+
+// console.log(rotateArray( ['a', 'b', 'c', 'd', 'e', 'f', 'g','‘h'], 3) );
+// console.log(rotateArray( ['a', 'b', 'c', 'd', 'e', 'f', 'g','‘h'], -2) );
+
+/* Task 5 */
+
+function invertObject(obj) {
+    let res = {};
+    for(let key in obj){
+        if(res.hasOwnProperty(obj[key])){
+            res[obj[key]] = [...res[obj[key]], key];
+        }else {
+            res[obj[key]] = key;
+        }
+    }
+    return res;
+}
+
+// console.log(invertObject({ a: '1', b: '2' }));
+// console.log(invertObject({ a: '1', b: '2', c: '2', d: '2'  }));
+
+
+/* Task 6 */
+
+function sortByPercentAndReadStatus(arr) {
+    return arr.sort((a, b) => {
+        if(b.readStatus){
+            if(a.readStatus){
+                if((a.percent - b.percent) >= 0){
+                    return -1;
+                }else{
+                    return 1
+                }
+            }else {
+                return 1
+            }
+        } else {
+            if(a.readStatus){
+                return -1
+            }else {
+                return 1
+            }
+        }
+    });
+}
+
+let readers = [
+    { book: "Catcher in the Rye", readStatus: true, percent: 40},
+    { book: "Catcher in the Rye", readStatus: true, percent: 110},
+    { book: "Animal Farm", readStatus: true, percent: 20},
+    { book: "Solaris", readStatus: false, percent: 90 },
+    { book: "The Fall", readStatus: true, percent: 50 },
+    { book: "White Nights", readStatus: false, percent: 60 } ,
+    { book: "White Nights", readStatus: false, percent: 10 } ,
+    { book: "After Dark", readStatus: true, percent: 70 }
+];
+
+console.log(sortByPercentAndReadStatus(readers));
+
