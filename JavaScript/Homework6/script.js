52070fc5a02d76122b8923f48312adea50d95bdd
RomanPoghosyan <roman.poghos@gmail.com>
2019-11-11 10:23:57 +0400

    JavaScript Homework 6

---
+++ JavaScript/Homework6/script.js
@@ -0,0 +1,79 @@
+/* Task 1 */
+function findPair(arr){
+    let largest = -Infinity;
+    for(let i = 0; i < arr.length - 1; i++){
+        if((arr[i] * arr[i + 1]) > largest){
+            largest = arr[i] * arr[i + 1];
+        }
+    }
+    return largest;
+}
+
+// console.log(findPair([2,1,4,5,2,9,1,7,3]));
+
+
+/* Task 2 */
+function findMissingNumbers(arr){
+    let count = 0;
+    arr.sort((a,b) => a - b);
+    for(let i = 0, num = arr[0]; num <= arr[arr.length - 1]; i++, num++){
+        if(num != arr[i]){
+            count++;
+            i--;
+        }
+    }
+    return count;
+}
+
+// console.log(findMissingNumbers([6,4,3,8,10,12]));
+
+
+/* Task 3 */
+function acronym(phrase) {
+    let words = phrase.split(" ");
+    let result = "";
+    words.forEach((word) => {result += word[0]});
+    return result;
+}
+
+// console.log(acronym("Hayastani Petakan Tntesagitakan Hamalsaran"));
+
+
+/* Task 4 */
+function subStringsOfLengthN(str, n) {
+    let result = [];
+    for(let i = 0; i < str.length - n + 1; i++){
+        result.push(str.substr(i, n));
+    }
+    return result;
+}
+
+// console.log(subStringsOfLengthN("4952151", 3));
+
+
+/* Task 5 */
+function findChilds(id, arr) {
+    let res = arr.filter((item) => item.parent == id);
+    return res;
+}
+
+function buildTreeLikeObject(arr, id = 0, res = {}){
+    res[id] = {};
+    let childs = findChilds(id, arr);
+    for(let i = 0; i < childs.length; i++){
+        buildTreeLikeObject(arr, childs[i].id, res[id]);
+    }
+    return res;
+}
+
+let treeNodes = [
+    {parent: null, id: 0},
+    {parent: 0, id: 1},
+    {parent: 0, id: 2},
+    {parent: 1, id: 3},
+    {parent: 1, id: 4},
+    {parent: 2, id: 5},
+    {parent: 4, id: 6},
+];
+
+// console.log( buildTreeLikeObject(treeNodes) );
